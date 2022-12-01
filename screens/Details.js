1d8e75d013f3130389e7c142d86556828bbb9c7f
abdul kareem <abdulak4t7@gmail.com>
2022-12-01 10:06:03 -0800

    created the two screen files

---
+++ screens/Details.js
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native-web"
+
+
+export const Details = () => {
+  return (
+    <Text>Details</Text>
+  )
+}
---
+++ screens/Home.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native"
+
+export const Home = () => {
+  return (
+    <Text>Home</Text>
+  )
+}
