4c44124dce443ccca3a9899462a208a2b4377525
abdul kareem <abdulak4t7@gmail.com>
2022-12-02 07:21:48 -0800

    created components and its files

---
+++ components/NFTCad.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NFTCad = () => {
+  return (
+    <View>
+      <Text>NFTCad</Text>
+    </View>
+  )
+}
+
+export default NFTCad
