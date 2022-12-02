b6d488334c8a02f2cf161052cd9ea322865fff17
abdul kareem <abdulak4t7@gmail.com>
2022-12-02 07:10:35 -0800

    added a constamts file and other few modification to home

--- screens/Home.js
+++ screens/Home.js
@@ -1,4 +1,7 @@
-import { Text } from "react-native"
+import { Text,View,SafeAreaView,FlatList } from "react-native";
+import { COLORS, NFTdata} from '../constants';
+
+import { useState } from "react";
 
 export const Home = () => {
   return (
