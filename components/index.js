3e60eb630b684036c9125d10001ce7049e791059
abdul kareem <abdulak4t7@gmail.com>
2022-12-02 13:32:14 -0800

    done with focusedsatusbar now working on home

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import { HomeHeader } from './HomeHeader';
+import { NFTCard } from './NFTCard';
+import { FocusedStatusBar } from './FocusedStatusBar';
+
+
+export default {
+    HomeHeader, NFTCard, FocusedStatusBar
+}
--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,41 @@
 import { Text,View,SafeAreaView,FlatList } from "react-native";
-import { COLORS, NFTdata} from '../constants';
-
 import { useState } from "react";
+
+import { COLORS, NFTData} from '../constants';
+import FocusedStatusBar from "../components/FocusedStatusBar";
+import HomeHeader from "../components/HomeHeader";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+
 
 export const Home = () => {
   return (
-    <Text>Home</Text>
+    <SafeAreaView style={{flex: 1}}>
+        <FocusedStatusBar background={COLORS.primary}/>
+
+        <View style={{ flex: 1 }} >
+            <View style={{ zIndex: 0 }} >
+                <FlatList 
+                data={NFTData}
+                renderItem={({item}) => <Text>{ item.name }</Text>}
+                keyExtractor={( item ) => item.id }
+                showsVerticalScrollIndicator={false}
+                ListHeaderComponent={<HomeHeader/>}
+                />
+            </View>
+
+            <View style={{
+                position: "absolute",
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                zIndex: -1
+            }}>
+                <View style={{ backgroundColor: COLORS.primary, height: '300px' }} />
+                <View style={{ backgroundColor: COLORS.white , flex:1 }} />
+            </View>
+        </View>
+    </SafeAreaView>
   )
 }
